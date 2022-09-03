526825069152a364dcbacf9b66f7bfcc0f7a3083
danielDeVita <danielitodevita@gmail.com>
2022-09-03 20:18:20 -0300

    server up

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-"hola"
+const express = require("express");
+const app = express();
+const path = require("path")
+const port = process.env.PORT || 3000;
+
+app.use(express.static("./public"));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//app.use((req, res, next)=>{res.status(404).render('not-found')});
+
+app.get("/", (req, res) => res.send("ginger"));
+
+app.listen(port, () => console.log(`server ${port} ok`));
