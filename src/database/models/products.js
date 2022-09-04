d72b8c4aefb8521a3440c1fddf2b8f3558815b87
danielDeVita <danielitodevita@gmail.com>
2022-09-04 17:32:12 -0300

    model done

---
+++ src/database/models/products.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "products";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: dataTypes.STRING(50)
+        },
+        description: {
+            type: dataTypes.STRING(300),
+        },
+        image: {
+            type: dataTypes.STRING(200)
+        }
+    };
+    let config = {
+        tableName: "products",
+        timestamps: false
+    };
+    const products = sequelize.define(alias, cols, config)
+    return products
+};
