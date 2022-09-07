48d2cd5d7e4cbadf69a7a3e040ad1e2cffb345c4
danielDeVita <danielitodevita@gmail.com>
2022-09-07 10:15:47 -0300

    cambie de funcion con promesas .then a una funcion async/await con try/catch

--- controllers.js
+++ controllers.js
@@ -1,7 +1,15 @@
 const db = require("./src/database/models");
 
 const controllers = {
-  home: (req, res) => db.products.findAll().then(products => res.render("home", { products })),
+  //home: (req, res) => db.products.findAll().then(products => res.render("home", { products })),
+  home: async (req, res) => {
+    try {
+      let products = await db.products.findAll();
+      res.render("home", { products })
+    } catch (err) {
+      console.error(err)
+    }
+  }
 };
 
 module.exports = controllers;
