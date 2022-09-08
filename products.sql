a177ad7aa6472f3cc78d94e47dfb696c4c51bc55
danielDeVita <danielitodevita@gmail.com>
2022-09-07 21:05:25 -0300

    new script with photos

--- products.sql
+++ products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Sep 07, 2022 at 11:35 PM
+-- Generation Time: Sep 08, 2022 at 12:04 AM
 -- Server version: 5.7.34
 -- PHP Version: 7.4.21
 
@@ -41,16 +41,16 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`id`, `name`, `description`, `category`, `image`) VALUES
 (1, 'Baby, I Love You', 'Mousse de frutos rojos sobre masa quebrada de cacao y centro bombón con corazón de frambuesa.', '', ''),
-(2, 'Bomba Golosa', 'Brownie acompañado de dulce de leche y merengue.', '', ''),
-(3, 'Cheesecake', 'Base de masa quebrada, crema de queso, y salsa de frutos rojos.', '', ''),
-(4, 'Chocotorta', 'Suave crema sobre las auténticas chocolinas.\r\n', '', ''),
-(5, 'Lemon Pie', 'Base de masa quebrada, crema de limón y merengue italiano.', '', ''),
-(6, ' Mousse de dulce de leche', 'Mousse de dulce de leche con biscuit de coco y maní.', '', ''),
-(7, 'Postre Ginger', 'Mousse de chocolate semiamargo sobre masa quebrada de jengibre y centro de maracuyá.', '', ''),
-(8, 'Postre Marilyn', 'Base de cacao intenso, mousse de chocolate blanco y centro de frambuesas.', '', ''),
-(9, 'Postre Oreo', 'Crema americana sobre las inigualables galletitas Oreo con salsa de dulce de leche.', '', ''),
-(10, 'Tiramisú', 'Clásico de mascarpone y vainillas caseras remojadas en almíbar de café.', '', ''),
-(11, 'Gingerito', 'Galletita de vainilla y mousse de chocolate con leche cubierto de cereal multicolor.', '', '');
+(2, 'Bomba Golosa', 'Brownie acompañado de dulce de leche y merengue.', '', 'bomba_golosa.jpg'),
+(3, 'Cheesecake', 'Base de masa quebrada, crema de queso, y salsa de frutos rojos.', '', 'cheesecake.jpg'),
+(4, 'Chocotorta', 'Suave crema sobre las auténticas chocolinas.\r\n', '', 'choco_torta.jpg'),
+(5, 'Lemon Pie', 'Base de masa quebrada, crema de limón y merengue italiano.', '', 'lemon_pie.jpg'),
+(6, ' Mousse de dulce de leche', 'Mousse de dulce de leche con biscuit de coco y maní.', '', 'mouse_dulce_leche.jpg'),
+(7, 'Postre Ginger', 'Mousse de chocolate semiamargo sobre masa quebrada de jengibre y centro de maracuyá.', '', 'postre_ginger.jpg'),
+(8, 'Postre Marilyn', 'Base de cacao intenso, mousse de chocolate blanco y centro de frambuesas.', '', 'marilyn.jpg'),
+(9, 'Postre Oreo', 'Crema americana sobre las inigualables galletitas Oreo con salsa de dulce de leche.', '', 'postre_oreo.jpg'),
+(10, 'Tiramisú', 'Clásico de mascarpone y vainillas caseras remojadas en almíbar de café.', '', 'tiramisu.jpg'),
+(11, 'Gingerito', 'Galletita de vainilla y mousse de chocolate con leche cubierto de cereal multicolor.', '', 'gingeritos.jpg');
 
 --
 -- Indexes for dumped tables
